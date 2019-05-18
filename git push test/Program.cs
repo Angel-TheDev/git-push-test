c3a0d8868aa77ccd3b0327ead2eccba970fedfa2
Angel-TheDev <angelantoinetted@yahoo.com>
2019-05-18 13:04:37 -0400

    first commit

---
+++ git push test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace git_push_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a git push test");
+        }
+    }
+}
